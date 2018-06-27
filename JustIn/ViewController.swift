371c3419a91f3115a279cc1503d02c061603110f
hemant89 <hemant.bhadak2000@gmail.com>
2018-06-27 19:23:29 +0530

    Second commit ever

--- JustIn/ViewController.swift
+++ JustIn/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("this is first commit")
+        print("this is second commit")
     }
 
     override func didReceiveMemoryWarning() {
