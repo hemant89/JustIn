504b7c2a30001cf1b723e39974db2ab253fe2a92
hemant89 <hemant.bhadak2000@gmail.com>
2018-06-27 20:01:04 +0530

    revert changes

--- JustIn/ViewController.swift
+++ JustIn/ViewController.swift
@@ -16,9 +16,8 @@ class ViewController: UIViewController {
         
         print("this is first commit")
         print("this is second commit")
-        print("this is third commit")
-        print("this is fourth commit")
-        print("this is fifth commit")
+        
+        print("This is new branch commit")
     }
 
     override func didReceiveMemoryWarning() {
