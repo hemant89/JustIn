3b1d02d2093eecbb341e2af9c5ba2a8ad76c9436
hemant89 <hemant.bhadak2000@gmail.com>
2018-06-27 19:24:57 +0530

    Fourth commit ever

--- JustIn/ViewController.swift
+++ JustIn/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         print("this is first commit")
         print("this is second commit")
         print("this is third commit")
+        print("this is fourth commit")
     }
 
     override func didReceiveMemoryWarning() {
